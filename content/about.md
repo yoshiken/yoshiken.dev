49ba113cf1a825d618dd1cac4432a1dc934892da
yoshiken <yskn.beer@gmail.com>
2022-05-15 05:42:30 +0900

    init

---
+++ content/about.md
@@ -0,0 +1,32 @@
+# About
+
+## これはなに
+
+yoshikenがプログラミングや日記、ポエムを書いていくページです。
+
+別に毎日更新とか圧倒的説明とか意識高い系ではなく思考の垂れ流しです。
+
+ソースコードは [https://github.com/yoshiken/yoshiken.dev](https://github.com/yoshiken/yoshiken.dev) に置いてあります。
+
+投稿内容は丸パクリじゃなきゃ特に問題ありません。引用や改変しての掲載は許諾も連絡も不要です。「一報くれると嬉しいな」ぐらいの温度感です。
+
+## Me
+
+- HN: yoshiken
+- Twitter: [https://twitter.com/yoshiken_tut](https://twitter.com/yoshiken_tut)
+- Github: [https://github.com/yoshiken](https://github.com/yoshiken)
+
+yoshikenという名前でインターネットを彷徨っています。
+
+小2あたりからパソコンを触って工業高校(情報科)→情報系Fラン大学→留年→大学院(修士)→社会人という学歴
+
+インフラが主戦場でバックエンドも人並み。フロントはできなくはないけど得意ではない。
+
+pythonで便利スクリプト作るの大好きマンです。
+
+年に一回は登壇と(執筆|寄稿)をしています。
+
+カンファレンススタッフもやったりしてます。
+
+## 連絡先
+[Twitter](https://twitter.com/yoshiken_tut)か[こちらのフォーム](https://forms.gle/HvXBKrz85BFaduCx8)まで。
---
+++ main.py
@@ -0,0 +1,29 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import os
+import markdown
+
+
+def index_page(env):
+    template = env.get_template("index/base.j2")
+    with open("output/index.html", mode='w') as f:
+        f.write(template.render())
+
+
+def about_page(env):
+    md = markdown.Markdown()
+    with open("content/about.md") as f:
+        s = f.read()
+        text = md.convert(s)
+    template = env.get_template("about/base.j2")
+    with open("output/about.html", mode='w') as f:
+        f.write(template.render({'body_text': text}))
+
+
+if __name__ == "__main__":
+    os.makedirs("output", exist_ok=True)
+    env = Environment(
+        loader=FileSystemLoader("template"),
+        autoescape=select_autoescape()
+    )
+    index_page(env)
+    about_page(env)
