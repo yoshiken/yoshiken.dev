17b5948e32b00d5a4c99a06a721e5e27fe5eaef9
yoshiken <yskn.beer@gmail.com>
2022-05-15 12:57:49 +0900

    add articles page convert

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
 import markdown
 import shutil
+import glob
 
 
 def index_page(env):
@@ -24,6 +25,21 @@ def cp_favicon():
     shutil.copyfile("./template/static/favicon.ico", "./docs/favicon.icon")
 
 
+def articles_page(env):
+    output_dir = "docs/articles"
+    os.makedirs(output_dir, exist_ok=True)
+    md = markdown.Markdown()
+    articles_list = glob.glob("./content/articles/*")
+    for article in articles_list:
+        template = env.get_template("articles/base.j2")
+        with open(article) as f:
+            s = f.read()
+            text = md.convert(s)
+            output_file_name = os.path.splitext(os.path.basename(article))[0] + ".html"
+        with open(output_dir + "/" + output_file_name, mode='w') as f:
+            f.write(template.render({'body_text': text}))
+
+
 if __name__ == "__main__":
     os.makedirs("docs", exist_ok=True)
     env = Environment(
@@ -33,3 +49,4 @@ if __name__ == "__main__":
     index_page(env)
     about_page(env)
     cp_favicon()
+    articles_page(env)
